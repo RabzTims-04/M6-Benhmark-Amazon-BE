640748dd03423f3f01392ee83cb7dca86e9860c3
Rabz92 <rabbiailyas92@gmail.com>
2021-08-10 14:13:24 +0200

    changes

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,21 +1,27 @@
-import s from "sequelize"
+import s from "sequelize";
 
-const { Sequelize } = s
-const { PGHOST, PGPASSWORD, PGUSER, PGPORT, PGDATABASE } = process.env
+const { Sequelize } = s;
+const { PGHOST, PGPASSWORD, PGUSER, PGPORT, PGDATABASE } = process.env;
 
 const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
-    host: PGHOST,
-    port: PGPORT,
-    dialect: "postgres"
-})
+  host: PGHOST,
+  port: PGPORT,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 sequelize
-    .authenticate()
-        .then(() => {
-            console.log("DB is authenticated");
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+  .authenticate()
+  .then(() => {
+    console.log("DB is authenticated");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-export default sequelize
+export default sequelize;
